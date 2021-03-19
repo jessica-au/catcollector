c146a73295978b0bcba4ee166e036a9d807b338e
jessica-au <AU.JESSICA.M@GMAIL.COM>
2021-03-19 15:21:40 -0700

    stuff

---
+++ main_app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse('<h1>Cat Collector</h1>')
+
+def about(request):
+    lorem_ipsum= "lorem ipsum dolor sit amet, etc"
+    return HttpResponse(lorem_ipsum)
+
+def contact(request):
+    return render(request, 'contact.html')
+
+    # 1. make a view function
+    # 2. make the html page
+    # 3. add the view to the urls.py inside of main_app.urls
+
+    # when i go to localhost:8000/contact
+
+    # django => urls => /contact => views.contact (runs function)
